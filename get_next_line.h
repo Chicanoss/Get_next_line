f0813fda860a79a474b838b5c4652d716a978d84
Robin Geral <rgeral@z1r3p5.42lyon.fr>
2021-11-29 12:13:52 +0100

    draft 2

--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/26 18:34:31 by rgeral            #+#    #+#             */
-/*   Updated: 2021/11/29 11:52:41 by rgeral           ###   ########.fr       */
+/*   Created: 2021/11/29 11:59:15 by rgeral            #+#    #+#             */
+/*   Updated: 2021/11/29 12:11:50 by rgeral           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+#endif
 
 #include <unistd.h>
 #include <stdio.h>
@@ -17,33 +26,8 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-char *ft_cpy_str(char *buffer, int	size)
-{
-	char	*str;
+char *get_next_line(int fd);
+int	ft_strlen(const char	*str);
+char	*ft_strrchr(const char *str, int c);
 
-	str = malloc((size + 1) * sizeof(char));
-	str = buffer;
-	printf("%s" , str);
-
-	return(str);
-}
-
-char *get_next_line(int fd)
-{
-	char buffer[BUFFER_SIZE + 1];
-	int	size;
-	char	*str;
-
-	size = read(fd, buffer, BUFFER_SIZE);
-	str = ft_cpy_str(buffer, size);
-	return(str);
-}
-
-int	main(void)
-{
-	int	fd;
-
-	fd = open("test", 0x000);
-	get_next_line(fd);
-	return(0);
-}
+#endif
